e58b42e3afa03bd12e87cbdf2bde189f58ff9327
Vasile Sambor <vasilesambor@gmail.com>
2020-02-17 21:53:07 +0100

    [#5] - WIP add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const ROCK_TYPE = 'rock';
+export const PAPER_TYPE = 'paper';
+export const SCISSORS_TYPE = 'scissors';
+
+// export const ROCK = 1;
+// export const PAPER = 2;
+// export const SCISSORS = 3;
+
+export const PLAYER_1_WON_RESULT = 1;
+export const PLAYER_2_WON_RESULT = 2;
+export const DRAW_RESULT = 3;
